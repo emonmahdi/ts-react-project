1f6c57322b4c7dd2c063045304fdabfd87a6b2f3
unknown <emonhowlader1997@gmail.com>
2022-04-14 17:28:58 +0600

    first ts react commit

---
+++ src/views/pages/TheLayout.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { Footer } from './common/Footer'
+import { Header } from './common/Header'
+
+export  const TheLayout = () => {
+  return (
+    <div>
+      <div>
+          <Header />
+      </div>
+      <main>
+          Body
+      </main>
+      <div>
+          <Footer />
+      </div>
+    </div>
+  )
+}
+
+ 
